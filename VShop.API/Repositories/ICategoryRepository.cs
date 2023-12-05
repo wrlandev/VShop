b066491399358d314d366d106d49c93d051da438
Wrlan.Dev <109324822+wrlandev@users.noreply.github.com>
2023-12-05 19:28:02 -0300

    Repositories

---
+++ VShop.API/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using VShop.API.Models;
+
+namespace VShop.API.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<IEnumerable<Category>> GetCategoriesProducts();
+        Task<Category> GetById(int id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Delete(int id);
+    }
+}
