d30026989aa3a555b4843fad70be8746f2377f20
Wrlan.Dev <109324822+wrlandev@users.noreply.github.com>
2023-12-19 20:23:26 -0300

    Project restarted

--- VShop.API/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VShop.API.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string? Name { get; set; }
-        public ICollection<Product> Products { get; set; }
-    }
-}
--- VShop.WEB/Services/Contracts/ICategoryService.cs
+++
@@ -1,8 +0,0 @@
-﻿using VShop.WEB.Models;
-
-namespace VShop.WEB.Services.Contracts;
-
-public interface ICategoryService
-{
-    Task<IEnumerable<CategoryViewModel>> GetAllCategories();
-}
